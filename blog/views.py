93a2aaaba8e0d5d1115f1f9d8b7ab9928f2c99bc
Sina <sinafateh1382@gmail.com>
2025-11-19 18:58:27 +0330

    feat(api): add serializers, views and URLs for BlogCategory and BlogPost

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Category, Post
+from .serializers import CategorySerializer, PostSerializer
 
-# Create your views here.
+# Categories
+class CategoryListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+# Posts
+class PostListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
